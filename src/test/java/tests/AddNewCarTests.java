333179e0db3fb36f80cd2cb19e03e959d97c8417
Maria Z <z0559882272@gmail.com>
2024-01-05 15:07:25 +0200

    photo

--- src/test/java/tests/AddNewCarTests.java
+++ src/test/java/tests/AddNewCarTests.java
@@ -4,6 +4,8 @@ package tests;
 
 import models.Car;
 import models.User;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -22,7 +24,7 @@ public void preConditions(){
 
     @Test
     public void addNewCarSuccessAll(){
-        int i = new Random().nextInt(1000)+1000;
+        int i = new Random().nextInt(10000)+1000;
         Car car = Car.builder()
                 .location("Tel Aviv, Israel")
                 .manufacture("Mazda")
@@ -31,14 +33,21 @@ public void preConditions(){
                 .fuel("Petrol")
                 .seats(4)
                 .carClass("C")
-                .carRegNumber("678-900-"+i)
+                .carRegNumber("678-900-"+i+"1")
                 .price(50)
                 .about("Very nice car")
                 .build();
 
         app.getHelperCar().openCarForm();
         app.getHelperCar().fillCarForm(car);
+        app.getHelperCar().attachPhoto("C:\\Users\\marii\\QA21_22\\QA21_22_IlCarro\\02-bugatti-cd-nardo-testing.jpg");
         app.getHelperCar().submit();
+        Assert.assertTrue(app.getHelperUser().getMessage().contains("added successful"));
+        Assert.assertEquals(app.getHelperUser().getMessage(), car.getManufacture()
+                + " "+car.getModel()
+                + " added successful");
+
+
     }
 
 
@@ -60,8 +69,15 @@ public void preConditions(){
         app.getHelperCar().openCarForm();
         app.getHelperCar().fillCarForm(car);
         app.getHelperCar().submit();
+        Assert.assertTrue(app.getHelperUser().getMessage().contains("added successful"));
+        Assert.assertEquals(app.getHelperUser().getMessage(), car.getManufacture()
+                + " "+car.getModel()
+                + " added successful");
     }
 
-
+@AfterMethod
+    public void postCondition(){
+    app.getHelperCar().returnToHome();
+}
 
 }
